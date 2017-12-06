5c39b99ac6d1dee27fb190dd4fe030fe13fe77b0
Alexander Ksenzov <alexander.ksenzov@gmail.com>
2017-12-06 12:28:52 +0200

    Fix bug with Signup step 4

--- src/components/user/Registration/Step4.js
+++ src/components/user/Registration/Step4.js
@@ -1,117 +1,124 @@
-import React, {Component, PropTypes} from 'react'
-import { connect } from 'react-redux'
-import { Link } from 'react-router'
-import autoBind from 'react-autobind'
-import { apiGetRequest, navigate, login, registrationStep4, setFormErrors } from 'actions'
-import { SETTINGS_LOGIN_URL } from 'services/api'
-import { Alert, Checkbox, ErrorBlock, Input } from 'components'
-import { formToObject } from 'services/helpers'
+import React, { Component, PropTypes } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router";
+import autoBind from "react-autobind";
+import {
+  apiGetRequest,
+  navigate,
+  login,
+  registrationStep4,
+  setFormErrors
+} from "actions";
+import { SETTINGS_LOGIN_URL } from "services/api";
+import { Alert, Checkbox, ErrorBlock, Input } from "components";
+import { formToObject } from "services/helpers";
 /**
  * js/app.js had method that automatically removes "toggled" class
  * from lc-block and div started to be invisible.
  * Removed this method from app.js
  */
-class Registration extends Component{
-    constructor(props){
-        super(props)
-        autoBind(this)
+class Registration extends Component {
+  constructor(props) {
+    super(props);
+    autoBind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    let { user, auth = {} } = nextProps;
+
+    if (auth.step4 === true || (user !== null && !user.registration_step)) {
+      this.props.navigate("/");
     }
+  }
 
-    componentWillReceiveProps(nextProps) {
-        let { auth = {} } = nextProps;
-        if (auth.step4 === true) {
-            this.props.navigate('/')
-        }
-    }
+  componentWillUnmount() {
+    this.props.setFormErrors("clear", null);
+  }
 
-    componentWillUnmount(){
-        this.props.setFormErrors('clear', null)
-    }
+  onSubmit(e) {
+    e.preventDefault();
+    this.props.setFormErrors("clear", null);
 
-    onSubmit(e){
+    let form = formToObject(e.target);
 
-        e.preventDefault()
-        this.props.setFormErrors('clear', null)
+    this.props.registrationStep4(form);
+  }
 
-        let form = formToObject(e.target)
+  render() {
+    const { errors, successMessage, settings } = this.props;
 
-        this.props.registrationStep4(form)
-    }
+    return (
+      <div className="login-content">
+        <div ref="block" className="lc-block toggled">
+          <h1
+            style={{
+              color: "white",
+              textShadow: "1px 1px 2px #666"
+            }}
+          >
+            Create a CoinStash Account
+          </h1>
 
+          <div className="lcb-form">
+            <form ref="form" onSubmit={this.onSubmit} target="">
+              <Input
+                name="ssn"
+                type="number"
+                max="9999"
+                wrapperClass="input-group m-b-20"
+                zmdi="zmdi-card"
+                placeholder="Last 4 of Social Security"
+                errors={errors}
+              />
 
-    render(){
-        const { errors, successMessage, settings } = this.props
+              <Input
+                name="date_of_birth"
+                type="date"
+                wrapperClass="input-group m-b-20"
+                zmdi="zmdi-calendar"
+                placeholder="Date of Birth MM/DD/YYYY"
+                errors={errors}
+              />
 
-        return (
-            <div className="login-content">
-                <div ref="block" className="lc-block toggled">
-                    <h1 style={{
-                        color: 'white',
-                        textShadow: '1px 1px 2px #666'
-                    }}>Create a CoinStash Account</h1>
+              <button type="submit" className="btn btn-success">
+                Continue
+              </button>
 
-                    <div className="lcb-form">
-
-                        <form ref="form" onSubmit={this.onSubmit} target="">
-
-                        <Input
-                            name="ssn"
-                            type="number"
-                            max="9999"
-                            wrapperClass="input-group m-b-20"
-                            zmdi="zmdi-card"
-                            placeholder="Last 4 of Social Security"
-                            errors={errors} />
-
-                        <Input
-                            name="date_of_birth"
-                            type="date"
-                            wrapperClass="input-group m-b-20"
-                            zmdi="zmdi-calendar"
-                            placeholder="Date of Birth MM/DD/YYYY"
-                            errors={errors} />
-
-                        <button
-                            type="submit"
-                            className="btn btn-success">
-                            Continue
-                        </button>
-
-                        {errors && <ErrorBlock errors={errors} />}
-
-                        </form>
-
-                    </div>
-                </div>
-            </div>
-        )
-    }
+              {errors && <ErrorBlock errors={errors} />}
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 Registration.propTypes = {
-    apiGetRequest: PropTypes.func,
-    errors: PropTypes.object,
-    isAuthenticated: PropTypes.bool,
-    login: PropTypes.func,
-    navigate: PropTypes.func,
-    registration: PropTypes.func,
-    setFormErrors: PropTypes.func,
-    settings: PropTypes.object,
-    successMessage: PropTypes.string
-}
+  apiGetRequest: PropTypes.func,
+  errors: PropTypes.object,
+  user: PropTypes.object,
+  isAuthenticated: PropTypes.bool,
+  login: PropTypes.func,
+  navigate: PropTypes.func,
+  registration: PropTypes.func,
+  setFormErrors: PropTypes.func,
+  settings: PropTypes.object,
+  successMessage: PropTypes.string
+};
 
-const mapStateToProps = (state) => ({
-    auth: state.auth,
-    isAuthenticated: state.auth.isAuthenticated,
-    errors: state.formErrors.registrationStep4,
-    settings: state.settingsLogin,
-    successMessage: state.auth.registrationSuccessMessage
-})
+const mapStateToProps = state => ({
+  auth: state.auth,
+  user: state.user.item,
+  isAuthenticated: state.auth.isAuthenticated,
+  errors: state.formErrors.registrationStep4,
+  settings: state.settingsLogin,
+  successMessage: state.auth.registrationSuccessMessage
+});
 
 export default connect(mapStateToProps, {
-    apiGetRequest,
-    login,
-    navigate,
-    registrationStep4,
-    setFormErrors
-})(Registration)
+  apiGetRequest,
+  login,
+  navigate,
+  registrationStep4,
+  setFormErrors
+})(Registration);
--- src/routes.js
+++ src/routes.js
@@ -1,87 +1,90 @@
-import React from 'react'
-import { Route, IndexRoute } from 'react-router'
+import React from "react";
+import { Route, IndexRoute } from "react-router";
 
 import {
-    AccountsPage,
-    AddBankPage,
-    AddDebtAccountPage,
-    AddLenderPage,
-    ConfigureAccountsPage,
-    CreateFundingSourcePage,
-    HomePage,
-    LoanCalculatorPage,
-    LoginPage,
-    LoginFacebookPage,
-    NotFoundPage,
-    RegistrationPage,
-    RegistrationConfirmPage,
-    ResetPasswordPage,
-    ResetPasswordRequestPage,
-    SettingsPage,
-    TestPage,
-    TransactionsPage,
-    TransfersPage,
-    UserNotConfirmedPage,
+  AccountsPage,
+  AddBankPage,
+  AddDebtAccountPage,
+  AddLenderPage,
+  ConfigureAccountsPage,
+  CreateFundingSourcePage,
+  HomePage,
+  LoanCalculatorPage,
+  LoginPage,
+  LoginFacebookPage,
+  NotFoundPage,
+  RegistrationPage,
+  RegistrationConfirmPage,
+  ResetPasswordPage,
+  ResetPasswordRequestPage,
+  SettingsPage,
+  TestPage,
+  TransactionsPage,
+  TransfersPage,
+  UserNotConfirmedPage,
+  RegistrationStep1,
+  RegistrationStep2,
+  RegistrationStep3,
+  RegistrationStep4,
+  UserProfilePage,
+  RegistrationParentPage,
+  DownLoadAppPage
+} from "pages";
 
-    RegistrationStep1,
-    RegistrationStep2,
-    RegistrationStep3,
-    RegistrationStep4,
-
-    UserProfilePage,
-    RegistrationParentPage,
-    DownLoadAppPage } from 'pages'
-
-
-import App from 'containers/App'
-import { requireAuth } from 'components/Auth'
-import { requireActiveAccount } from 'components/ActiveAccount'
-import { requirePrimaryAccount } from 'components/PrimaryAccount'
-import { requireSignupCompleted } from 'components/SignupCompleted'
-import { requireSignupStepsCompleted } from 'components/SignupStepsCompleted'
-
+import App from "containers/App";
+import { requireAuth } from "components/Auth";
+import { requireActiveAccount } from "components/ActiveAccount";
+import { requirePrimaryAccount } from "components/PrimaryAccount";
+import { requireSignupCompleted } from "components/SignupCompleted";
+import { requireSignupStepsCompleted } from "components/SignupStepsCompleted";
 
 export default (
-    <div>
-        <Route component={LoginPage} path="/login" />
-        <Route component={LoginFacebookPage} path="/login_facebook" />
-        <Route component={RegistrationParentPage} path="/registration_parent" />
-        <Route component={RegistrationStep1} path="/registration" />
-        <Route component={requireAuth(RegistrationStep2)} path="/registration/2" />
-        <Route component={requireAuth(RegistrationStep3)} path="/registration/3" />
-        <Route component={requireAuth(RegistrationStep4)} path="/registration/4" />
-        <Route component={RegistrationConfirmPage} path="/confirm" />
-        <Route component={ResetPasswordRequestPage} path="/forgot_password" />
-        <Route component={ResetPasswordPage} path="/reset" />
+  <div>
+    <Route component={LoginPage} path="/login" />
+    <Route component={LoginFacebookPage} path="/login_facebook" />
+    <Route component={RegistrationParentPage} path="/registration_parent" />
+    <Route component={RegistrationStep1} path="/registration" />
+    <Route component={requireAuth(RegistrationStep2)} path="/registration/2" />
+    <Route component={requireAuth(RegistrationStep3)} path="/registration/3" />
+    <Route component={requireAuth(RegistrationStep4)} path="/registration/4" />
+    <Route component={RegistrationConfirmPage} path="/confirm" />
+    <Route component={ResetPasswordRequestPage} path="/forgot_password" />
+    <Route component={ResetPasswordPage} path="/reset" />
 
-        <Route component={requireAuth(UserNotConfirmedPage)} path="/not_confirmed" />
-        <Route component={requireAuth(DownLoadAppPage)} path="/download_app" />
-        <Route
-            component={
-                requireAuth(
-                    requireSignupStepsCompleted(
-                        requireActiveAccount(
-                            requirePrimaryAccount(
-                                requireSignupCompleted(App)))))}
-            path="/"
-        >
+    <Route
+      component={requireAuth(UserNotConfirmedPage)}
+      path="/not_confirmed"
+    />
+    <Route component={requireAuth(DownLoadAppPage)} path="/download_app" />
+    <Route
+      component={requireAuth(
+        requireSignupStepsCompleted(
+          requireActiveAccount(
+            requirePrimaryAccount(requireSignupCompleted(App))
+          )
+        )
+      )}
+      path="/"
+    >
+      <IndexRoute component={HomePage} />
+      <Route component={AccountsPage} path="/accounts" />
+      <Route component={AddBankPage} path="/add_bank" />
+      <Route component={AddLenderPage} path="/add_lender" />
+      <Route component={ConfigureAccountsPage} path="/configure_accounts" />
+      <Route
+        component={CreateFundingSourcePage}
+        path="/create_funding_source"
+      />
+      <Route component={LoanCalculatorPage} path="/loan_calculator" />
+      <Route component={SettingsPage} path="/settings" />
+      <Route component={TestPage} path="/test_page" />
+      <Route component={TransactionsPage} path="/transactions" />
+      <Route component={UserProfilePage} path="/user_profile" />
+    </Route>
 
-            <IndexRoute component={HomePage} />
-            <Route component={AccountsPage} path="/accounts" />
-            <Route component={AddBankPage} path="/add_bank" />
-            <Route component={AddLenderPage} path="/add_lender" />
-            <Route component={ConfigureAccountsPage} path="/configure_accounts" />
-            <Route component={CreateFundingSourcePage} path="/create_funding_source" />
-            <Route component={LoanCalculatorPage} path="/loan_calculator" />
-            <Route component={SettingsPage} path="/settings" />
-            <Route component={TestPage} path="/test_page" />
-            <Route component={TransactionsPage} path="/transactions" />
-            <Route component={UserProfilePage} path="/user_profile" />
-        </Route>
-
-        <Route component={NotFoundPage} path="*" />
-    </div>
-)
+    <Route component={NotFoundPage} path="*" />
+  </div>
+);
 
 // Functionality for debt accounts
 // <Route component={AddDebtAccountPage} path="/add_debt_account" />
