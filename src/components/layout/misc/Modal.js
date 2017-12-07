3c88e41ccfe738953567900d9f6a44d6acf0bbb6
Alexander Ksenzov <alexander.ksenzov@gmail.com>
2017-12-07 12:27:30 +0200

    Add Terms modal window

--- src/components/layout/misc/Modal.js
+++ src/components/layout/misc/Modal.js
@@ -1,80 +1,77 @@
-import React, {Component, PropTypes} from 'react'
-import classNames from 'classnames'
-
+import React, { Component, PropTypes } from "react";
+import classNames from "classnames";
 
 /**
  * onClickClose - passed from parent (show/hide controls from parent).
  * footer - additional node to footer.
  */
-class Modal extends Component{
-    static get defaultProps() {
-        return {
-            footer: null,
-            showCloseButton: true,
-        }
-    }
+class Modal extends Component {
+  static get defaultProps() {
+    return {
+      footer: null,
+      showCloseButton: true
+    };
+  }
 
-    constructor(props){
-        super(props)
-        this.onClickClose = this.onClickClose.bind(this)
-    }
+  constructor(props) {
+    super(props);
+    this.onClickClose = this.onClickClose.bind(this);
+  }
 
-    onClickClose(){
-        this.props.onClickClose()
-    }
+  onClickClose() {
+    this.props.onClickClose();
+  }
 
-    render(){
-        const { children, footer, title, visible, showCloseButton } = this.props
+  render() {
+    const { children, footer, title, visible, showCloseButton } = this.props;
 
-        let style = visible ? {'display': 'block', 'paddingRight': '15px'} : {'display': 'none'}
-        
-        let cn = classNames('modal fade', {'in': visible})
+    let style = visible
+      ? { display: "block", paddingRight: "15px" }
+      : { display: "none" };
 
-        let modalBodyStyle = {
-            maxHeight: '80%',
-            overflowY: 'scroll'    
-        }
-        
+    let cn = classNames("modal fade", { in: visible });
 
-        return (
-            <div className={cn} style={style}>
-                <div className="modal-dialog" style={modalBodyStyle}>
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h4 className="modal-title">{title}</h4>
-                        </div>
-                        <div className="modal-body">
-                            {children}
-                        </div>
-                        <div className="modal-footer">
-                            {footer}
+    let modalBodyStyle = {
+      maxHeight: "80%",
+      overflowY: "scroll"
+    };
 
-                            {showCloseButton &&
-                                <button
-                                    onClick={this.onClickClose}
-                                    type="button"
-                                    className="btn btn-link waves-effect"
-                                    data-dismiss="modal">
-                                    {'Close'}
-                                </button>
-                            }
-                        </div>
-                    </div>
-                </div>
+    return (
+      <div className={cn} style={style}>
+        <div className="modal-dialog" style={modalBodyStyle}>
+          <div className="modal-content">
+            <div className="modal-header">
+              <h4 className="modal-title">{title}</h4>
             </div>
-        )
-    }
+            <div className="modal-body">{children}</div>
+            <div className="modal-footer">
+              {footer}
+
+              {showCloseButton && (
+                <button
+                  onClick={this.onClickClose}
+                  type="button"
+                  className="btn btn-link waves-effect"
+                  data-dismiss="modal"
+                >
+                  {"Close"}
+                </button>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 Modal.propTypes = {
-    children: PropTypes.node,
-    footer: PropTypes.node,
-    onClickClose: PropTypes.func,
-    showCloseButton: PropTypes.bool,
-    title: PropTypes.string,
-    visible: PropTypes.bool
-}
+  children: PropTypes.node,
+  footer: PropTypes.node,
+  onClickClose: PropTypes.func,
+  showCloseButton: PropTypes.bool,
+  title: PropTypes.string,
+  visible: PropTypes.bool
+};
 
-export default Modal
-
-
+export default Modal;
--- src/components/user/Registration/Step1.js
+++ src/components/user/Registration/Step1.js
@@ -1,139 +1,228 @@
-import React, {Component, PropTypes} from 'react'
-import { connect } from 'react-redux'
-import { Link } from 'react-router'
-import autoBind from 'react-autobind'
-import { apiGetRequest, navigate, login, registrationStep1, setFormErrors } from 'actions'
-import { SETTINGS_LOGIN_URL } from 'services/api'
-import { Alert, Checkbox, ErrorBlock, Input } from 'components'
-import { formToObject } from 'services/helpers'
+import React, { Component, PropTypes } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router";
+import autoBind from "react-autobind";
+import {
+  apiGetRequest,
+  navigate,
+  login,
+  registrationStep1,
+  setFormErrors
+} from "actions";
+import { SETTINGS_LOGIN_URL } from "services/api";
+import { Alert, Checkbox, ErrorBlock, Input, Modal } from "components";
+import { formToObject } from "services/helpers";
 /**
  * js/app.js had method that automatically removes "toggled" class
  * from lc-block and div started to be invisible.
  * Removed this method from app.js
  */
-class Registration extends Component{
-    constructor(props){
-        super(props)
-        autoBind(this)
+class Registration extends Component {
+  constructor(props) {
+    super(props);
+    autoBind(this);
 
-        this.state = {
-            email: null,
-            password: null
-        }
+    this.state = {
+      form: null,
+      showModal: false,
+      agree: false
+    };
+  }
+
+  componentWillMount() {
+    if (this.props.isAuthenticated) {
+      this.props.navigate("/");
     }
+    this.props.apiGetRequest(
+      "settings_login",
+      { useToken: false },
+      SETTINGS_LOGIN_URL
+    );
+  }
 
-    componentWillMount(){
-        if (this.props.isAuthenticated){
-            this.props.navigate('/')
-        }
-        this.props.apiGetRequest('settings_login', {useToken: false}, SETTINGS_LOGIN_URL)
-    }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.isAuthenticated) nextProps.navigate("/");
+  }
 
-    componentWillReceiveProps(nextProps){
-        if (nextProps.isAuthenticated)
-            nextProps.navigate('/')
+  componentWillUnmount() {
+    this.props.setFormErrors("clear", null);
+  }
 
-        if (nextProps.successMessage !== null){
-            this.refs.form.reset()
-            // Auto login after signup
-            const { email, password } = this.state
-            this.props.login(email, password)
-        }
-    }
+  onSubmit(e) {
+    e.preventDefault();
+    this.props.setFormErrors("clear", null);
 
-    componentWillUnmount(){
-        this.props.setFormErrors('clear', null)
-    }
+    let form = formToObject(e.target);
 
-    onSubmit(e){
+    this.setState({ form, showModal: true });
+  }
 
-        e.preventDefault()
-        this.props.setFormErrors('clear', null)
+  onAccept(e) {
+    e.preventDefault();
+    let { form } = this.state;
+    this.setState({ showModal: false });
+    this.props.registrationStep1(form);
+  }
 
-        let form = formToObject(e.target)
+  onChange(e) {
+    this.setState({
+      accept: e.target.checked
+    });
+  }
 
-        this.setState({email: form.email, password: form.password})
-        this.props.registrationStep1(form)
-    }
+  onClickClose(e) {
+    this.setState({ showModal: false });
+  }
 
+  render() {
+    let { showModal = false, accept = false } = this.state;
+    const { errors, successMessage, settings } = this.props;
 
-    render(){
-        const { errors, successMessage, settings } = this.props
-
-        return (
-            <div className="login-content">
-                <div ref="block" className="lc-block toggled">
-                    <h1 style={{
-                        color: 'white',
-                        textShadow: '1px 1px 2px #666'
-                    }}>Create account</h1>
-
-                    <div className="lcb-form">
-
-                        <form ref="form" onSubmit={this.onSubmit} target="">
-
-                        <Input
-                            name="email"
-                            wrapperClass="input-group m-b-20"
-                            zmdi="zmdi-email"
-                            placeholder="Email Address"
-                            errors={errors} />
-
-                        <Input
-                            name="password"
-                            type="password"
-                            wrapperClass="input-group m-b-20"
-                            zmdi="zmdi-male"
-                            placeholder="Password"
-                            errors={errors} />
-
-                        <div className="m-b-10">
-                            Already have an account?
-                            <Link to="/login" className="m-l-5">
-                                <span>{'Click Here'}</span>
-                            </Link>
-                        </div>
-
-                        <button
-                            type="submit"
-                            className="btn btn-success">
-                            Continue
-                        </button>
-
-                        {errors && <ErrorBlock errors={errors} />}
-
-                        </form>
-
-                    </div>
+    return (
+      <div className="login-content">
+        <div ref="block" className="lc-block toggled">
+          <h1
+            style={{
+              color: "white",
+              textShadow: "1px 1px 2px #666"
+            }}
+          >
+            Create account
+          </h1>
+          {showModal ? (
+            <Modal
+              onClickClose={this.onClickClose}
+              showCloseButton={true}
+              visible
+              title="Terms of Service"
+              footer={
+                <button
+                  type="button"
+                  onClick={this.onAccept}
+                  disabled={!accept}
+                  className="btn btn-success"
+                >
+                  Continue
+                </button>
+              }
+            >
+              <div className="modal-window-terms">
+                <div className="modal-window-text">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel
+                  eveniet nihil tempore ea ipsum, minus magni dolor. Iure sed
+                  expedita, omnis totam ex tempore rerum reprehenderit
+                  repellendus similique. Incidunt aliquam est voluptates eveniet
+                  atque porro sequi odio voluptatum aut aperiam, eos ipsam nihil
+                  officiis, earum totam ad temporibus molestiae quibusdam cum.
+                  Dolor nulla inventore itaque, et sequi voluptas nemo amet
+                  quisquam, nostrum dolorem totam laudantium dolores. Rem
+                  repellendus soluta explicabo, labore excepturi iure
+                  consectetur voluptatibus tenetur obcaecati! Corrupti impedit
+                  laudantium, in, alias dolore corporis, velit quae natus,
+                  voluptatum tenetur nisi. Ab beatae ipsum ea sunt laudantium,
+                  culpa odio dolores, assumenda, quos, numquam unde alias totam
+                  ullam libero eveniet ratione suscipit possimus debitis fuga
+                  explicabo architecto sint aut! Eligendi ipsum quam veniam,
+                  ratione qui alias, nemo rerum explicabo debitis temporibus
+                  saepe sit accusantium earum dolorum assumenda laudantium
+                  corporis excepturi similique, molestiae nostrum. Laboriosam
+                  ipsa suscipit voluptatem natus deserunt repudiandae veritatis
+                  dolore fuga, tenetur vero recusandae consequuntur modi
+                  aliquid, iusto eaque facere vitae molestiae praesentium
+                  numquam architecto quo quisquam, quod. Ducimus laudantium ex
+                  temporibus nostrum eius, quibusdam possimus neque eveniet,
+                  quisquam explicabo officiis assumenda reprehenderit qui
+                  inventore, quae nemo magni? Culpa cumque accusamus similique,
+                  vero aut praesentium, officia repudiandae minus voluptatibus
+                  modi odit consequatur sint corrupti possimus quae atque, ipsum
+                  voluptatum dolores recusandae molestiae, eius veniam! Vero
+                  laborum, officia esse tempora ipsa. Eius aut aperiam, ratione
+                  libero sit doloribus accusantium pariatur eos, officia
+                  expedita in natus vitae voluptatem, harum inventore tempora,
+                  omnis velit at deleniti quos! Incidunt aliquid similique
+                  quidem quisquam amet consectetur ab pariatur itaque dolor quia
+                  ratione, placeat, animi, perferendis, fugiat quaerat aliquam.
+                  Neque earum illo est facere qui consequuntur. Quaerat eaque
+                  numquam omnis perspiciatis harum, praesentium tempore facilis
+                  non, totam minus excepturi! Totam in maxime nulla nesciunt
+                  aliquid! Consequatur pariatur quos corporis amet quasi unde
+                  dignissimos inventore dolor minus.
                 </div>
-            </div>
-        )
-    }
+                <div className="modal-window-checkbox">
+                  <input
+                    name="agree"
+                    id="id_agree"
+                    type="checkbox"
+                    onChange={this.onChange}
+                  />
+                  <label htmlFor="id_agree">Click here to agree</label>
+                </div>
+              </div>
+            </Modal>
+          ) : null}
+
+          <div className="lcb-form">
+            <form ref="form" onSubmit={this.onSubmit} target="">
+              <Input
+                name="email"
+                wrapperClass="input-group m-b-20"
+                zmdi="zmdi-email"
+                placeholder="Email Address"
+                errors={errors}
+              />
+
+              <Input
+                name="password"
+                type="password"
+                wrapperClass="input-group m-b-20"
+                zmdi="zmdi-male"
+                placeholder="Password"
+                errors={errors}
+              />
+
+              <div className="m-b-10">
+                Already have an account?
+                <Link to="/login" className="m-l-5">
+                  <span>{"Click Here"}</span>
+                </Link>
+              </div>
+
+              <button type="submit" className="btn btn-success">
+                Continue
+              </button>
+
+              {errors && <ErrorBlock errors={errors} />}
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 Registration.propTypes = {
-    apiGetRequest: PropTypes.func,
-    errors: PropTypes.object,
-    isAuthenticated: PropTypes.bool,
-    login: PropTypes.func,
-    navigate: PropTypes.func,
-    registration: PropTypes.func,
-    setFormErrors: PropTypes.func,
-    settings: PropTypes.object,
-    successMessage: PropTypes.string
-}
+  apiGetRequest: PropTypes.func,
+  errors: PropTypes.object,
+  isAuthenticated: PropTypes.bool,
+  login: PropTypes.func,
+  navigate: PropTypes.func,
+  registration: PropTypes.func,
+  setFormErrors: PropTypes.func,
+  settings: PropTypes.object,
+  successMessage: PropTypes.string
+};
 
-const mapStateToProps = (state) => ({
-    isAuthenticated: state.auth.isAuthenticated,
-    errors: state.formErrors.registrationStep1,
-    settings: state.settingsLogin,
-    successMessage: state.auth.registrationSuccessMessage
-})
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated,
+  errors: state.formErrors.registrationStep1,
+  settings: state.settingsLogin,
+  successMessage: state.auth.registrationSuccessMessage
+});
 
 export default connect(mapStateToProps, {
-    apiGetRequest,
-    login,
-    navigate,
-    registrationStep1,
-    setFormErrors
-})(Registration)
+  apiGetRequest,
+  login,
+  navigate,
+  registrationStep1,
+  setFormErrors
+})(Registration);
