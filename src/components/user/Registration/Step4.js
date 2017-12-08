185de45d2ab35b822a5e55f35ed7e1018adca007
Alexander Ksenzov <alexander.ksenzov@gmail.com>
2017-12-08 12:43:52 +0200

    Add "?" button after SSN field

--- src/components/user/Registration/Step4.js
+++ src/components/user/Registration/Step4.js
@@ -10,7 +10,7 @@ import {
   setFormErrors
 } from "actions";
 import { SETTINGS_LOGIN_URL } from "services/api";
-import { Alert, Checkbox, ErrorBlock, Input } from "components";
+import { Alert, Checkbox, ErrorBlock, Input, Modal } from "components";
 import { formToObject } from "services/helpers";
 /**
  * js/app.js had method that automatically removes "toggled" class
@@ -21,6 +21,10 @@ class Registration extends Component {
   constructor(props) {
     super(props);
     autoBind(this);
+
+    this.state = {
+      showModal: false
+    };
   }
 
   componentWillReceiveProps(nextProps) {
@@ -44,8 +48,23 @@ class Registration extends Component {
     this.props.registrationStep4(form);
   }
 
+  showModalWindow(e) {
+    if (e) {
+      e.preventDefault();
+    }
+    this.setState({ showModal: true });
+  }
+
+  hideModalWindow(e) {
+    if (e) {
+      e.preventDefault();
+    }
+    this.setState({ showModal: false });
+  }
+
   render() {
     const { errors, successMessage, settings } = this.props;
+    let { showModal } = this.state;
 
     return (
       <div className="login-content">
@@ -59,17 +78,78 @@ class Registration extends Component {
             Create a CoinStash Account
           </h1>
 
+          {showModal ? (
+            <Modal
+              onClickClose={this.hideModalWindow}
+              showCloseButton={true}
+              visible
+              title="SSN"
+            >
+              <div className="modal-window-terms">
+                <div className="modal-window-text">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel
+                  eveniet nihil tempore ea ipsum, minus magni dolor. Iure sed
+                  expedita, omnis totam ex tempore rerum reprehenderit
+                  repellendus similique. Incidunt aliquam est voluptates eveniet
+                  atque porro sequi odio voluptatum aut aperiam, eos ipsam nihil
+                  officiis, earum totam ad temporibus molestiae quibusdam cum.
+                  Dolor nulla inventore itaque, et sequi voluptas nemo amet
+                  quisquam, nostrum dolorem totam laudantium dolores. Rem
+                  repellendus soluta explicabo, labore excepturi iure
+                  consectetur voluptatibus tenetur obcaecati! Corrupti impedit
+                  laudantium, in, alias dolore corporis, velit quae natus,
+                  voluptatum tenetur nisi. Ab beatae ipsum ea sunt laudantium,
+                  culpa odio dolores, assumenda, quos, numquam unde alias totam
+                  ullam libero eveniet ratione suscipit possimus debitis fuga
+                  explicabo architecto sint aut! Eligendi ipsum quam veniam,
+                  ratione qui alias, nemo rerum explicabo debitis temporibus
+                  saepe sit accusantium earum dolorum assumenda laudantium
+                  corporis excepturi similique, molestiae nostrum. Laboriosam
+                  ipsa suscipit voluptatem natus deserunt repudiandae veritatis
+                  dolore fuga, tenetur vero recusandae consequuntur modi
+                  aliquid, iusto eaque facere vitae molestiae praesentium
+                  numquam architecto quo quisquam, quod. Ducimus laudantium ex
+                  temporibus nostrum eius, quibusdam possimus neque eveniet,
+                  quisquam explicabo officiis assumenda reprehenderit qui
+                  inventore, quae nemo magni? Culpa cumque accusamus similique,
+                  vero aut praesentium, officia repudiandae minus voluptatibus
+                  modi odit consequatur sint corrupti possimus quae atque, ipsum
+                  voluptatum dolores recusandae molestiae, eius veniam! Vero
+                  laborum, officia esse tempora ipsa. Eius aut aperiam, ratione
+                  libero sit doloribus accusantium pariatur eos, officia
+                  expedita in natus vitae voluptatem, harum inventore tempora,
+                  omnis velit at deleniti quos! Incidunt aliquid similique
+                  quidem quisquam amet consectetur ab pariatur itaque dolor quia
+                  ratione, placeat, animi, perferendis, fugiat quaerat aliquam.
+                  Neque earum illo est facere qui consequuntur. Quaerat eaque
+                  numquam omnis perspiciatis harum, praesentium tempore facilis
+                  non, totam minus excepturi! Totam in maxime nulla nesciunt
+                  aliquid! Consequatur pariatur quos corporis amet quasi unde
+                  dignissimos inventore dolor minus.
+                </div>
+              </div>
+            </Modal>
+          ) : null}
+
           <div className="lcb-form">
             <form ref="form" onSubmit={this.onSubmit} target="">
-              <Input
-                name="ssn"
-                type="number"
-                max="9999"
-                wrapperClass="input-group m-b-20"
-                zmdi="zmdi-card"
-                placeholder="Last 4 of Social Security"
-                errors={errors}
-              />
+              <div className="input-group m-b-20" style={{ display: "flex" }}>
+                <Input
+                  name="ssn"
+                  type="number"
+                  max="9999"
+                  wrapperClass="flex-container-full-width"
+                  zmdi="zmdi-card"
+                  placeholder="Last 4 of Social Security"
+                  errors={errors}
+                />
+                <button
+                  onClick={this.showModalWindow}
+                  className="btn btn-warning"
+                >
+                  ?
+                </button>
+              </div>
 
               <Input
                 name="date_of_birth"
