e8853a259723dde450af0bdda599a4e49ae13f15
Alexander Ksenzov <alexander.ksenzov@gmail.com>
2017-12-08 13:57:31 +0200

    Add Google location button

--- src/components/user/Registration/Step3.js
+++ src/components/user/Registration/Step3.js
@@ -10,9 +10,13 @@ import {
   setFormErrors
 } from "actions";
 import { SETTINGS_LOGIN_URL } from "services/api";
-import { Alert, Checkbox, ErrorBlock, Input, Select } from "components";
+import { Alert, Checkbox, ErrorBlock, Input, Select, Modal } from "components";
 import { formToObject, getGeoposition } from "services/helpers";
 import { API_ROOT_URL } from "store/configureStore";
+import Script from "react-load-script";
+
+const GOOGLE_API_KEY = "AIzaSyAJmIm7I7nR2yWVwa9I-8YyJSm1RCFfFu0";
+
 /**
  * js/app.js had method that automatically removes "toggled" class
  * from lc-block and div started to be invisible.
@@ -24,7 +28,8 @@ class Registration extends Component {
     autoBind(this);
 
     this.state = {
-      states: []
+      states: [],
+      showModal: false
     };
   }
 
@@ -60,32 +65,121 @@ class Registration extends Component {
     this.props.registrationStep3(form);
   }
 
-  clickOnLocationButton(e) {
-    e.preventDefault();
-    getGeoposition()
-      .then(geo => {
-        let GOOGLE_API_KEY = "AIzaSyAJmIm7I7nR2yWVwa9I-8YyJSm1RCFfFu0",
-          lat = geo.coords.latitude,
-          lon = geo.coords.longitude;
-        return fetch(
-          `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&key=${GOOGLE_API_KEY}`
-        )
-          .then(r => r.json())
-          .then(response => {
-            console.log(response);
-          })
-          .catch(error => {
-            console.log(error);
-          });
-      })
-      .catch(error => {
-        alert(error.message);
-      });
+  // clickOnLocationButton(e) {
+  //   e.preventDefault();
+  //   getGeoposition()
+  //     .then(geo => {
+  //       let GOOGLE_API_KEY = "AIzaSyAJmIm7I7nR2yWVwa9I-8YyJSm1RCFfFu0",
+  //         lat = geo.coords.latitude,
+  //         lon = geo.coords.longitude;
+  //       return fetch(
+  //         `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&key=${GOOGLE_API_KEY}`
+  //       )
+  //         .then(r => r.json())
+  //         .then(response => {
+  //           console.log(response);
+  //         })
+  //         .catch(error => {
+  //           console.log(error);
+  //         });
+  //     })
+  //     .catch(error => {
+  //       alert(error.message);
+  //     });
+  // }
+
+  showModalWindow(e) {
+    if (e) e.preventDefault();
+    this.setState({ showModal: true });
+  }
+
+  hideModalWindow(e) {
+    if (e) e.preventDefault();
+    this.setState({ showModal: false });
+  }
+
+  onScriptError() {}
+
+  onScriptLoad() {
+    let map = new google.maps.Map(document.getElementById("map"), {
+      center: { lat: 37.3002752813443, lng: -119.8828125 },
+      zoom: 5
+    });
+
+    map.addListener("click", e => {
+      this.hideModalWindow();
+      let lat = e.latLng.lat(),
+        lng = e.latLng.lng();
+
+      return fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${GOOGLE_API_KEY}`
+      )
+        .then(r => r.json())
+        .then(response => {
+          console.log(response);
+          let results = response.results;
+
+          // Street address
+
+          let street = results.filter(
+            item => item.types[0] == "street_address"
+          )[0];
+          let street_number = street.address_components.filter(
+            item => item.types[0] === "street_number"
+          )[0].long_name;
+          let route = street.address_components.filter(
+            item => item.types[0] === "route"
+          )[0].long_name;
+          document.getElementsByName(
+            "address1"
+          )[0].value = `${route}, ${street_number}`;
+
+          // Zip code
+
+          let zipcode = results
+            .filter(item => item.types[0] === "postal_code")[0]
+            .address_components.filter(
+              item => item.types[0] === "postal_code"
+            )[0].long_name;
+          document.getElementsByName("postal_code")[0].value = zipcode;
+
+          // City
+
+          let city = results
+            .filter(
+              item =>
+                item.types[0] === "locality" && item.types[1] === "political"
+            )[0]
+            .address_components.filter(
+              item =>
+                item.types[0] === "locality" && item.types[1] === "political"
+            )[0].long_name;
+          document.getElementsByName("city")[0].value = city;
+
+          // State
+
+          let state = results
+            .filter(
+              item =>
+                item.types[0] === "administrative_area_level_1" &&
+                item.types[1] === "political"
+            )[0]
+            .address_components.filter(
+              item =>
+                item.types[0] === "administrative_area_level_1" &&
+                item.types[1] === "political"
+            )[0].short_name;
+          document.getElementsByName("state")[0].value = state;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    });
   }
 
   render() {
     const { errors, successMessage, settings } = this.props;
-    let { states } = this.state;
+    let { states, showModal } = this.state;
 
     return (
       <div className="login-content">
@@ -99,16 +193,33 @@ class Registration extends Component {
             Create a CoinStash Account
           </h1>
 
+          {showModal ? (
+            <Modal
+              onClickClose={this.hideModalWindow}
+              showCloseButton={true}
+              visible
+              title="Select your location"
+            >
+              <div>
+                <Script
+                  url={`https://maps.googleapis.com/maps/api/js?key=${GOOGLE_API_KEY}`}
+                  onError={this.onScriptError}
+                  onLoad={this.onScriptLoad}
+                />
+                <div style={{ height: 250 }} id="map" />
+              </div>
+            </Modal>
+          ) : null}
+
           <div className="lcb-form">
             <form ref="form" onSubmit={this.onSubmit} target="">
-              {/*
-                        <button
-                            type="button"
-                            onClick={this.clickOnLocationButton}
-                            className="btn btn-primary m-b-20">
-                            Get Address from Google
-                        </button>
-                        */}
+              <button
+                type="button"
+                onClick={this.showModalWindow}
+                className="btn btn-primary m-b-20"
+              >
+                Get Address from Google
+              </button>
 
               <Input
                 name="address1"
