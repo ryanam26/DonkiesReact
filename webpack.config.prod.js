9f0aadfea4e5e076946bf05692997d7fd1c61217
Alexander Ksenzov <alexander.ksenzov@gmail.com>
2017-12-21 13:18:58 +0200

    Improve webpack config

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,56 +1,85 @@
-var path = require("path");
-var webpack = require("webpack");
-var HtmlWebpackPlugin = require("html-webpack-plugin");
-
-var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: __dirname + "/src/index.html",
-  hash: true,
-  filename: "index.html",
-  inject: "body"
-});
+const path = require("path");
+const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
   entry: ["./src/index.js"],
 
   output: {
-    path: "dist",
+    path: path.resolve(__dirname, "dist"),
     filename: "index_bundle.js",
     publicPath: "/"
   },
 
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
-        loaders: ["babel"],
-        include: __dirname + "/src",
-        exclude: /node_modules/
+        loaders: ["babel-loader"],
+        include: path.resolve(__dirname, "src")
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract(
+          "style-loader",
+          "css-loader!resolve-url!sass-loader?sourceMap"
+        )
       },
       {
         test: /\.css$/,
-        loader: "style-loader!css-loader",
-        include: __dirname + "/src",
-        exclude: /node_modules/
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+      },
+      {
+        test: /\.woff2?$|\.ttf$|\.eot$|\.svg$|\.png|\.jpe?g|\.gif$/,
+        loader: "file-loader"
       }
     ]
   },
 
   resolve: {
-    root: [path.resolve("./src"), path.resolve("./node_modules")],
-    extensions: ["", ".js"]
+    modules: [
+      path.resolve(__dirname, "src"),
+      path.resolve(__dirname, "node_modules")
+    ],
+    extensions: [".js", ".css", ".scss"]
   },
 
   plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.NoErrorsPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        screw_ie8: true
-      },
-      ie8: false,
-      safari10: false,
-      warnings: false
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new UglifyJsPlugin({
+      uglifyOptions: {
+        // compress: {
+        //   warnings: false,
+        //   conditionals: true,
+        //   unused: true,
+        //   comparisons: true,
+        //   sequences: true,
+        //   dead_code: true,
+        //   evaluate: true,
+        //   if_return: true,
+        //   join_vars: true
+        // },
+        // output: {
+        //   comments: false
+        // }
+      }
     }),
-    HTMLWebpackPluginConfig
+    new ExtractTextPlugin("css/style_bundle.css", {
+      allChunks: true
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html"),
+      hash: true,
+      filename: "index.html",
+      inject: "body",
+      minify: {
+        collapseWhitespace: true,
+        collapseInlineTagWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true
+      }
+    })
   ]
 };
