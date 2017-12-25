002a115eea6fe681cf061d52935d5caf9852f659
Alexander Ksenzov <alexander.ksenzov@gmail.com>
2017-12-25 16:51:39 +0200

    Improve webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -3,20 +3,44 @@ const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
-let plugins = [
-  new HtmlWebpackPlugin({
-    template: path.resolve(__dirname, "assets", "html", "index.html"),
-    hash: true,
-    filename: "index.html",
-    inject: "body"
-  })
-];
+let plugins = [];
+let devtool = "";
 
 if (process.env.NODE_ENV === "production") {
-  plugins += [new UglifyJsPlugin()];
+  plugins = [
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new UglifyJsPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "assets", "html", "index.html"),
+      hash: true,
+      filename: "index.html",
+      inject: "body",
+      minify: {
+        collapseWhitespace: true,
+        collapseInlineTagWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true
+      }
+    }),
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify("production")
+    })
+  ];
+} else {
+  devtool = "source-map";
+  plugins = [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "assets", "html", "index.html"),
+      hash: true,
+      filename: "index.html",
+      inject: "body"
+    })
+  ];
 }
 
 module.exports = {
+  devtool: devtool,
+
   entry: [path.resolve(__dirname, "assets", "js", "index.js")],
 
   output: {
